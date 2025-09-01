21115302dac9cbf4d8a1863e8b0ca1de52681d1f
mo <mauricetoneyjr@gmail.com>
2025-09-01 11:48:58 -0500

    - adds functionality for enter key

--- src/main/java/texteditor/controller/EditorController.java
+++ src/main/java/texteditor/controller/EditorController.java
@@ -44,7 +44,12 @@ public class EditorController {
             } else if (event.getCode() == KeyCode.HOME) {
                 cursor.moveHome();
                 modelChanged = true;
-            } else if (event.getCode().isLetterKey() || event.getCode().isDigitKey() || event.getText().length() == 1) {
+            } else if (event.getCode() == KeyCode.ENTER) {
+                document.insertText(cursor.getPosition(), "\n");
+                cursor.moveRight();
+                modelChanged = true;
+            }
+            else if (event.getCode().isLetterKey() || event.getCode().isDigitKey() || event.getText().length() == 1) {
                 document.insertText(cursor.getPosition(), event.getText());
                 cursor.moveRight();
                 modelChanged = true;
